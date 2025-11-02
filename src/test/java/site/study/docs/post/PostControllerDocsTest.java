5b08661a325ceeae21a80508be8fd37d3a60f7ae
kelly6bf <kelly6bf@gmail.com>
2025-11-03 00:19:44 +0900

    step02 : 게시글 API 문서 생성

--- src/test/java/site/study/docs/post/PostControllerDocsTest.java
+++ src/test/java/site/study/docs/post/PostControllerDocsTest.java
@@ -8,6 +8,8 @@ import site.study.docs.util.PostTestDataUtil;
 import site.study.docs.util.RestDocsSupport;
 import site.study.post.application.PostService;
 import site.study.post.application.dto.CreatePostRequestDto;
+import site.study.post.application.dto.LikeRequestDto;
+import site.study.post.application.dto.UpdatePostRequestDto;
 import site.study.post.domain.PostPublicationState;
 import site.study.post.ui.PostController;
 
@@ -17,6 +19,9 @@ import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -74,5 +79,123 @@ public class PostControllerDocsTest extends RestDocsSupport {
                         .description("작성한 게시글 ID")
                         .type(JsonFieldType.NUMBER))
                 ));
+    }
+
+    @DisplayName("게시글 수정 API")
+    @Test
+    void updatePost() throws Exception {
+        // Given
+        final Long postId = 1L;
+        final UpdatePostRequestDto request = new UpdatePostRequestDto(
+            1L,
+            "이제 팔친님들만 볼 수 있음 ㅋ",
+            PostPublicationState.ONLY_FOLLOWED
+        );
+
+        given(postService.updatePost(any(), any())).willReturn(PostTestDataUtil.createPostByCUpdatePostRequestDto(request));
+
+        // When & Then
+        mockMvc.perform(
+                patch("/api/post/{postId}", postId)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(convertToJson(request)))
+            .andExpect(status().isOk())
+            .andDo(createDocument(
+                pathParameters(
+                    parameterWithName("postId")
+                        .description("수정할 게시글 ID")
+                        .attributes(constraints("내가 작성한 게시글 ID만 입력 가능"), pathVariableExample(postId))
+                ),
+                requestFields(
+                    fieldWithPath("userId")
+                        .description("게시글을 수정하는 회원 ID")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(constraints("존재하는 회원 ID만 입력 가능")),
+                    fieldWithPath("content")
+                        .description("게시글 내용")
+                        .type(JsonFieldType.STRING)
+                        .attributes(constraints("5 ~ 500 길이의 문자열만 입력 가능")),
+                    fieldWithPath("state")
+                        .description("게시글 공개 상태")
+                        .type(JsonFieldType.STRING)
+                        .attributes(constraints("존재하는 옵션값만 입력 가능"))
+                        .attributes(options("PUBLIC(전체 공개)", "ONLY_FOLLOWED(팔로워들만 공개)", "PRIVATE(비공개)"))),
+                responseFields(
+                    fieldWithPath("code")
+                        .description("API 응답 코드")
+                        .type(JsonFieldType.NUMBER),
+                    fieldWithPath("message")
+                        .description("API 응답 메시지")
+                        .type(JsonFieldType.STRING),
+                    fieldWithPath("value")
+                        .description("수정한 게시글 ID")
+                        .type(JsonFieldType.NUMBER))
+            ));
+    }
+
+    @DisplayName("게시글 좋아요 생성 API")
+    @Test
+    void likePost() throws Exception {
+        // Given
+        final LikeRequestDto request = new LikeRequestDto(3L, 1L);
+
+        // When & Then
+        mockMvc.perform(
+                post("/api/post/like")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(convertToJson(request)))
+            .andExpect(status().isOk())
+            .andDo(createDocument(
+                requestFields(
+                    fieldWithPath("targetId")
+                        .description("좋아요를 생성할 게시글 ID")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(constraints("좋아요를 누를 수 있는 상태의 게시글 ID만 입력 가능")),
+                    fieldWithPath("userId")
+                        .description("좋아요를 생성하는 회원 ID")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(constraints("게시글 작성자 외 해당 게시글에 좋아요를 누르지 않은 사용자 ID만 입력 가능"))
+                ),
+                responseFields(
+                    fieldWithPath("code")
+                        .description("API 응답 코드")
+                        .type(JsonFieldType.NUMBER),
+                    fieldWithPath("message")
+                        .description("API 응답 메시지")
+                        .type(JsonFieldType.STRING)
+            )));
+    }
+
+    @DisplayName("게시글 좋아요 삭제 API")
+    @Test
+    void unlikePost() throws Exception {
+        // Given
+        final LikeRequestDto request = new LikeRequestDto(3L, 1L);
+
+        // When & Then
+        mockMvc.perform(
+                post("/api/post/unlike")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(convertToJson(request)))
+            .andExpect(status().isOk())
+            .andDo(createDocument(
+                requestFields(
+                    fieldWithPath("targetId")
+                        .description("좋아요를 삭제할 게시글 ID")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(constraints("좋아요를 삭제 할 수 있는 상태의 게시글 ID만 입력 가능")),
+                    fieldWithPath("userId")
+                        .description("좋아요를 생성하는 회원 ID")
+                        .type(JsonFieldType.NUMBER)
+                        .attributes(constraints("삭제할 좋아요를 생성한 사용자 ID만 입력 가능"))
+                ),
+                responseFields(
+                    fieldWithPath("code")
+                        .description("API 응답 코드")
+                        .type(JsonFieldType.NUMBER),
+                    fieldWithPath("message")
+                        .description("API 응답 메시지")
+                        .type(JsonFieldType.STRING)
+                )));
     }
 }
--- src/test/java/site/study/docs/util/PostTestDataUtil.java
+++ src/test/java/site/study/docs/util/PostTestDataUtil.java
@@ -2,6 +2,7 @@ package site.study.docs.util;
 
 import site.study.common.domain.PositiveIntegerCounter;
 import site.study.post.application.dto.CreatePostRequestDto;
+import site.study.post.application.dto.UpdatePostRequestDto;
 import site.study.post.domain.Post;
 import site.study.post.domain.content.PostContent;
 import site.study.user.domain.User;
@@ -10,6 +11,17 @@ import site.study.user.domain.UserInfo;
 public class PostTestDataUtil {
 
     public static Post createPostByCreatePostRequestDto(final CreatePostRequestDto dto) {
+        final User user = new User(dto.userId(), new UserInfo("kelly", "https://image-kelly.site"));
+        return new Post(
+            1L,
+            user,
+            new PostContent(dto.content()),
+            new PositiveIntegerCounter(),
+            dto.state()
+        );
+    }
+
+    public static Post createPostByCUpdatePostRequestDto(final UpdatePostRequestDto dto) {
         final User user = new User(dto.userId(), new UserInfo("kelly", "https://image-kelly.site"));
         return new Post(
             1L,
